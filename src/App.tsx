21568ff8742cf461fab0863e9555af05b203944b
Francis Lagares <francis.lagares@gmail.com>
2021-02-12 20:44:41 +0100

    feat: create SummaryForm component

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './App.css';
-
-const App = (): JSX.Element => {
-  return <div className='app'></div>;
-};
-
-export default App;
---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SummaryForm from '../pages/summary/SummaryForm';
+import './App.css';
+
+const App = (): JSX.Element => {
+  return (
+    <div className='app'>
+      <SummaryForm />
+    </div>
+  );
+};
+
+export default App;
