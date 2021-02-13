4f5348023c9dca1264f380bf5cc1da2120a2ba47
Francis Lagares <francis.lagares@gmail.com>
2021-02-13 21:05:17 +0100

    feat: implement popover functionality

--- src/pages/summary/SummaryForm.tsx
+++ src/pages/summary/SummaryForm.tsx
@@ -1,15 +1,27 @@
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Popover from 'react-bootstrap/Popover';
 
 const SummaryForm = (): JSX.Element => {
   const [tcChecked, setTcChecked] = useState(false);
 
+  const popover = (
+    <Popover id='termsandconditions-popover'>
+      No ice cream will actually be delivered
+    </Popover>
+  );
+
   const checkboxLabel = (
     <span>
-      I agree to <span style={{ color: 'blue' }}>Terms and Conditions</span>
+      I agree to
+      <OverlayTrigger placement='right' overlay={popover}>
+        <span style={{ color: 'blue' }}> Terms and Conditions</span>
+      </OverlayTrigger>
     </span>
   );
+
   return (
     <Form>
       <Form.Group controlId='terms-and-conditions'>
@@ -20,7 +32,7 @@ const SummaryForm = (): JSX.Element => {
           label={checkboxLabel}
         />
       </Form.Group>
-      <Button variant='primary' type='submit' disabled={!tcChecked}>
+      <Button variant='outline-dark' type='submit' disabled={!tcChecked}>
         Confirm order
       </Button>
     </Form>
